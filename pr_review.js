adf2ef9392c88ac5cd310cf0eac6bf24d6546039
hulmgulm <hulmgulm@users.noreply.github.com>
2022-04-20 13:42:48 +0200

    Update pr_review.js

--- pr_review.js
+++ pr_review.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Enterprise Github PR Highlighting
 // @namespace    https://github.com/hulmgulm/tampermonkey
-// @version      0.6.3
+// @version      0.6.4
 // @description  Highlight the PRs which are ready to get reviewed
 // @author       hulmgulm
 // @include      /https://github.*
@@ -13,14 +13,15 @@
 // ==/UserScript==
 
 const prHandling = () => {
-  if (document.getElementsByTagName("body")[0].getAttribute('prfinished') === 'true') {
+  console.log(document.getElementById('repo-content-pjax-container')?.getAttribute('prfinished'));
+  if (document.getElementById('repo-content-pjax-container')?.getAttribute('prfinished') === 'true') {
     // no need to run again
     return;
   }
 
   console.log('Running PR handling ... ');
 
-  const hovercard_subject_tag = document.querySelector('[name="hovercard-subject-tag"]').attributes['content'].value;
+  const hovercard_subject_tag = document.querySelector('[name="hovercard-subject-tag"]').attributes.content.value;
   const current_path = new URL(document.documentURI).pathname;
 
   const colorIssues = issues => {
@@ -35,13 +36,13 @@ const prHandling = () => {
       let opened_by = issue.querySelector(`[class*="opened-by"]`);
 
       /* load hovercard, containing target_branch */
-      GM.xmlHttpRequest ({
-        method: "GET",
-        url: data_hovercard_url + "?subject=" + hovercard_subject_tag + "&current_path=" + current_path,
+      GM.xmlHttpRequest({
+        method: 'GET',
+        url: `${data_hovercard_url}?subject=${hovercard_subject_tag}&current_path=${current_path}`,
         headers: {
           'X-Requested-With': 'XMLHttpRequest'
         },
-        onload: function(response) {
+        onload(response) {
           if (response.status === 200) {
             const target_branch = response.responseText.match(/class="commit-ref[^>]*>([^<]*)</i)[1].trim();
             const span = document.createElement('span');
@@ -57,48 +58,48 @@ const prHandling = () => {
       });
 
       /* get merge details */
-      GM.xmlHttpRequest ({
-          method: "GET",
-          url: pr_url + "/show_partial?partial=pull_requests%2Fmerging",
-          onload: function(response) {
-            if (response.status === 200) {
-              if (response.responseText.includes('This branch has conflicts that must be resolved')) {
-                const span = document.createElement('span');
-                span.appendChild(document.createTextNode('Merge Conflict!'));
-                span.style = 'margin:0 4px;--label-r:117;--label-g:6;--label-b:50;--label-h:336;--label-s:90;--label-l:24;';
-                span.classList.add('IssueLabel');
-                span.classList.add('hx_IssueLabel');
-                opened_by.before(span);
-              }
-            } else {
-              console.log('Unable to get merge details:', response.status, response.statusText, response.readyState, response.responseHeaders, response.responseText, response.finalUrl);
+      GM.xmlHttpRequest({
+        method: 'GET',
+        url: `${pr_url}/show_partial?partial=pull_requests%2Fmerging`,
+        onload(response) {
+          if (response.status === 200) {
+            if (response.responseText.includes('This branch has conflicts that must be resolved')) {
+              const span = document.createElement('span');
+              span.appendChild(document.createTextNode('Merge Conflict!'));
+              span.style = 'margin:0 4px;--label-r:117;--label-g:6;--label-b:50;--label-h:336;--label-s:90;--label-l:24;';
+              span.classList.add('IssueLabel');
+              span.classList.add('hx_IssueLabel');
+              opened_by.before(span);
             }
+          } else {
+            console.log('Unable to get merge details:', response.status, response.statusText, response.readyState, response.responseHeaders, response.responseText, response.finalUrl);
           }
+        }
       });
 
       /* get pr details to get reviewers */
-      GM.xmlHttpRequest ({
-          method: "GET",
-          url: pr_url,
-          onload: function(response) {
-            if (response.status === 200) {
-                const regexp = /class=["']reviewers-status-icon[^"']*["']*[^>]*aria-label=["']([^"']*)["']/g;
-                const rawReviews = [...response.responseText.matchAll(regexp)];
-                const target = issue.querySelector(`[class*="opened-by"]`).parentElement;
-                rawReviews.forEach( review => {
-                  if (review[1] !== 'Re-request review' && !review[1].includes('is a code owner')) {
-                      const span = document.createElement('span');
-                      span.appendChild(document.createTextNode(review[1].replace('approved these changes','✅').replace('requested changes','❌').replace('Awaiting requested review from','🟠')));
-                      span.style = 'margin:0 4px;';
-                      span.classList.add('IssueLabel');
-                      span.classList.add('hx_IssueLabel');
-                      target.after(span);
-                  }
-                });
-            } else {
-              console.log('Unable to get pr details:', response.status, response.statusText, response.readyState, response.responseHeaders, response.responseText, response.finalUrl);
-            }
+      GM.xmlHttpRequest({
+        method: 'GET',
+        url: pr_url,
+        onload(response) {
+          if (response.status === 200) {
+            const regexp = /class=["']reviewers-status-icon[^"']*[^>]*aria-label=["']([^"']*)["']/g;
+            const rawReviews = [...response.responseText.matchAll(regexp)];
+            const target = issue.querySelector(`[class*="opened-by"]`).parentElement;
+            rawReviews.forEach(review => {
+              if (review[1] !== 'Re-request review' && !review[1].includes('is a code owner')) {
+                const span = document.createElement('span');
+                span.appendChild(document.createTextNode(review[1].replace('approved these changes', '✅').replace('requested changes', '❌').replace('Awaiting requested review from', '🟠')));
+                span.style = 'margin:0 4px;';
+                span.classList.add('IssueLabel');
+                span.classList.add('hx_IssueLabel');
+                target.after(span);
+              }
+            });
+          } else {
+            console.log('Unable to get pr details:', response.status, response.statusText, response.readyState, response.responseHeaders, response.responseText, response.finalUrl);
           }
+        }
       });
 
       const draft = issue.querySelectorAll(`[aria-label="Open draft pull request"]`);
@@ -130,12 +131,11 @@ const prHandling = () => {
     });
   };
 
-
   const issues = [...document.querySelectorAll(`[id^="issue"]`)];
   if (issues.length > 0) {
     colorIssues(issues);
   }
-  document.getElementsByTagName("body")[0].setAttribute('prfinished', 'true');
+  document.getElementById('repo-content-pjax-container').setAttribute('prfinished', 'true');
 };
 
 waitForKeyElements(`[data-ga-click*="New pull request"]`, prHandling);
